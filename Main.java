6f128991df3e85a43d38e01ba372760a5e480807
fikriadinarta <fikriadinarta04@gmail.com>
2021-04-20 12:43:43 +0700

    tes

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (fikriadinarta)
+ * @version (0.1)
+ */
+public class Main
+{
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        // op1
+        private float op1;
+        public float op2;
+    }
+
+    /**
+     * constructor for objects of class calculator
+     * @param float, float inisialisasi nilai dari op1 dan op2
+     * 
+     */
+    public calculator(float op1, float op2)
+    {
+        
+    }
+}
