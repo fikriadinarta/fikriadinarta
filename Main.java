b69cca5237eb362a928b807c7f489b86d850e6d0
fikriadinarta <fikriadinarta04@gmail.com>
2021-04-20 13:34:52 +0700

    update

--- Main.java
+++ Main.java
@@ -1,30 +1,26 @@
-
 /**
  * Write a description of class Main here.
  *
  * @author (fikriadinarta)
  * @version (0.1)
- */
-public class Main
-{
-
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // op1
-        private float op1;
-        public float op2;
-    }
-
-    /**
-     * constructor for objects of class calculator
-     * @param float, float inisialisasi nilai dari op1 dan op2
-     * 
-     */
-    public calculator(float op1, float op2)
-    {
+ */public class Main{
+    
+    public static void main(String args[]){
+        // menciptakan objek baru dari class kalkulator
+        kalkulator kalkul = new kalkulator();
         
+        kalkul.name = "Operator ";
+
+        //author
+        System.out.println("--- OPERATOR DASAR PADA JAVA ---");
+        //inputan oprator yg dijalankan
+        System.out.println("nilai 1 = "+ kalkul.a);
+        System.out.println("nilai 1 = "+ kalkul.b);
+        //memanggil atribut hasil
+        System.out.println(kalkul.name + "penjumlahan 4 + 5 = "+ kalkul.penjumlahan);
+        System.out.println(kalkul.name + "pengurangan 4 - 5 = "+ kalkul.pengurangan);
+        System.out.println(kalkul.name + "perkalian 4 * 5 = "+ kalkul.perkalian);
+        System.out.println(kalkul.name + "pembagian 4 / 5 = "+ kalkul.pembagian);        
     }
+
 }
---
+++ kalkulator.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * Write a description of class kalkulator here.
+ *
+ * @author (fikri adinarta)
+ * @version (0.1)
+ */
+public class kalkulator{
+
+    String name;
+    
+    int a = 4;
+    int b = 5;
+    
+    int perkalian = a * b;
+    int pembagian = a / b;
+    int penjumlahan = a + b;
+    int pengurangan = a - b;
+    
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     
+    public int add(){
+        return x + y;
+    }*/
+}
